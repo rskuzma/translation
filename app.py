147eeef786ae68b254fb4b10c5346138f5fe06d5
Richard Kuzma <kuzmarichards@gmail.com>
2022-04-19 12:13:46 -0400

    slight changes to app

--- app.py
+++ app.py
@@ -32,13 +32,25 @@ with st.sidebar:
 
 # @st.cache
 def load_hf_models(language:str):
+    """Load NLP machine translation models from HuggingFace.
+
+    This application uses HuggingFace, a high-level API for open-source NLP models.
+    This function calls the `translate` module for actual loading of the models
+
+    Args:
+        language (str): Language of text to be translated. Comes from Streamlit button user input
+
+    Returns:
+        tokenizer: HuggingFace tokenizer that will turn text into machine-readable tokens
+        model: HuggingFace model doing the translations
+    """    
     if language == 'arabic':
         tokenizer, model = translate.arabic()
     elif language == 'russian':
         tokenizer, model = translate.russian()
     elif language == 'french':
         tokenizer, model = translate.french()
-    # st.write(f'loaded model: {language}')
+        
     return tokenizer, model
     
 
@@ -64,40 +76,63 @@ topics = st.multiselect(label='Pick potential topics',
 
 
 if run:
-    if time_it:
-        t_start = time.time()
-    st.subheader('Step 1: Translating...')
+    ### Translate
+    st.subheader('Step 1: Translate...')
+    st.write('Loading translation model')
+    translate_model_load_start = time.time()
     tokenizer, model = load_hf_models(input_language)
+    translate_model_load_time = time.time() - translate_model_load_start 
+    
     st.write('Input text: ' + input_text)
+    
+    st.write('Translating text')
+    translate_time_start = time.time()
     english_text = translate.translate_one_line(input_text, model, tokenizer)
+    translate_time = time.time() - translate_time_start
+
+
     st.write('English translation: ' + english_text)
-    st.text('\n\n')
+    st.text('\n\n\n\n\n')
 
+
+    ### Topic Classify
     if input_classify == 'Yes':
-        st.subheader('Step 2: Classifying...')
-        st.write('Loading classification model...')
+        st.subheader('Step 2: Classify Topic(s)...')
+        st.write('\n\n')
+        classify_model_load_start = time.time()
         zero_shot_classifier = classify.load_zero_shot_classifier()
-        st.text('\n\n')
+        classify_model_load_time = time.time() - classify_model_load_start
+        
 
-
+        classify_time_start = time.time()
         classification_dict_raw = classify.multi_label_classify(input_text=english_text, 
                                                             potential_labels=topics,
                                                             classifier=zero_shot_classifier)
         classification_dict_slim = {k: v for k, v in classification_dict_raw.items() if k in ['labels', 'scores']}
-        
+        classify_time = time.time() - classify_time_start
         st.table(data=classification_dict_slim)
-        # st.write(classification_dict)
+
 
     if input_ner == 'Yes':
-        st.subheader('Step 3: Extracting Entities...')
+        st.subheader('Step 3: Extract Entities...')
+        ner_time_start = time.time()
         doc = ner.extract_entitites_to_doc(english_text)
         entity_dict = ner.entities_to_dict(doc)
+        ner_time = time.time() - ner_time_start
         st.write(entity_dict)
         st.text('\n\n')
     
-    t_end = time.time()
-    total_time = round(t_end-t_start, 2)
-    st.write(f'Time: {total_time} seconds')
+    if time_it == 'Yes':
+        st.subheader('Times:')
+        st.write(f'Time to load translation model: {round(translate_model_load_time, 2)} seconds')
+        st.write(f'Time to translate: {round(translate_time, 2)} seconds\n')
+        st.write(f'Time to load topic classifier model: {round(classify_model_load_time,2)} seconds')
+        st.write(f'Time to classify topics: {round(classify_time, 2)} seconds\n')
+        st.write(f'Time to extract entities: {round(ner_time, 2)} seconds\n')
+        
+        st.write(f'Time to load models: {round(translate_model_load_time + classify_model_load_time,2)} seconds')
+        st.write(f'Time to make predictions: {round(translate_time + classify_time + ner_time, 2)} seconds')
+        
 
 
 
